bd7aa110da0c4a4d54e8b94511c6a53c8c20756e
Listener <Listener@localhost>
2014-03-14 10:47:43 +0400

    migrate to yandex-disk utility instead webdav

--- yandex_backup.py
+++ yandex_backup.py
@@ -1,23 +1,19 @@
 # -*- coding: utf-8 -*-
-import easywebdav
-from conf.webdav import projects, backup_root_dir, webdav_config, email_config
-from processors import WebdavProcessor
+from conf.yandex import projects, backup_root_dir, dst_root_dir, email_config
+from processors import YandexProcessor
 
-
-processor = WebdavProcessor(**{
+processor = YandexProcessor(**{
     'backup_root_dir': backup_root_dir,
-    'dst_root_dir': '/backup/',
-    'webdav': easywebdav.connect(**webdav_config),
+    'dst_root_dir': dst_root_dir,
     'email_config': email_config,
 })
-
 
 # проходим по проектам
 for project in projects:
     processor.process_project(project_data=project)
 # заливаем файлы на яндекс диск
-processor.copy_result_files_webdav(webdav_config=webdav_config)
-# удаляем старые файлы
-processor.delete_old_backup_webdav()
+processor.copy_result_files_yandex()
+# удаляем старые бэкапы на яндексе
+processor.delete_old_backup_yandex()
 # удаляем все директории бэкапов кроме последней
 processor.delete_old_backup()
